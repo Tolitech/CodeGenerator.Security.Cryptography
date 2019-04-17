0294b622c08e837b4599fa1ea38e8a72f7d459c4
Eduardo Tolino <etolino@msn.com>
2019-04-17 10:19:06 -0300

    adding classes.

---
+++ Cryptography/AesCryptography.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Tolitech.CodeGenerator.Security.Cryptography
+{
+    public sealed class AesCryptography
+    {
+        /// <summary>
+        /// Method to encrypt a text.
+        /// </summary>
+        /// <param name="plainText">plain text</param>
+        /// <param name="key">key</param>
+        /// <param name="iv">iv</param>
+        /// <returns>Encrypted text</returns>
+        public static string Encrypt(string plainText, string key, string iv)
+        {
+            string data = null;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+
+                        data = Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Method to decrypt a text.
+        /// </summary>
+        /// <param name="encryptedText">text</param>
+        /// <param name="key">key</param>
+        /// <param name="iv">iv</param>
+        /// <returns>Decrypted text</returns>
+        public static string Decrypt(string encryptedText, string key, string iv)
+        {
+            string data = null;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(encryptedText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            data = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Method to encrypt a file.
+        /// </summary>
+        /// <param name="plainFile">plain file</param>
+        /// <param name="key">key</param>
+        /// <param name="iv">iv</param>
+        /// <returns>Encrypted file</returns>
+        public static byte[] Encrypt(byte[] plainFile, string key, string iv)
+        {
+            byte[] encryptedFile = null;
+
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = Convert.FromBase64String(key);
+                aes.IV = Convert.FromBase64String(iv);
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream plain = new MemoryStream(plainFile))
+                {
+                    using (MemoryStream encrypted = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(encrypted, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (var originalByteStream = new MemoryStream(plainFile))
+                            {
+                                int data;
+
+                                while ((data = originalByteStream.ReadByte()) != -1)
+                                    cs.WriteByte((byte)data);
+                            }
+                        }
+
+                        encryptedFile = encrypted.ToArray();
+                    }
+                }
+            }
+
+            return encryptedFile;
+        }
+
+        /// <summary>
+        /// Method to decrypt a file.
+        /// </summary>
+        /// <param name="encryptedFile">encrypted file</param>
+        /// <param name="key">key</param>
+        /// <param name="iv">iv</param>
+        /// <returns>Decrypted file</returns>
+        public static byte[] Decrypt(byte[] encryptedFile, string key, string iv)
+        {
+            byte[] plainFile = null;
+
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = Convert.FromBase64String(key);
+                aes.IV = Convert.FromBase64String(iv);
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream encrypted = new MemoryStream(encryptedFile))
+                {
+                    using (MemoryStream plain = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(encrypted, decryptor, CryptoStreamMode.Read))
+                        {
+                            int data;
+
+                            while ((data = cs.ReadByte()) != -1)
+                                plain.WriteByte((byte)data);
+                        }
+
+                        // reset position in prep for reading.
+                        plain.Position = 0;
+                        plainFile = ConvertToByteArray(plain);
+                    }
+                }
+            }
+
+            return plainFile;
+        }
+
+        public static string GenerateKey()
+        {
+            string key = "";
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.GenerateKey();
+                key = Convert.ToBase64String(aesAlg.Key);
+            }
+
+            return key;
+        }
+
+        public static string GenerateIV()
+        {
+            string IV = "";
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.GenerateIV();
+                IV = Convert.ToBase64String(aesAlg.IV);
+            }
+
+            return IV;
+        }
+
+        private static byte[] ConvertToByteArray(Stream stream)
+        {
+            byte[] buffer = new byte[16 * 1024];
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    ms.Write(buffer, 0, read);
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ Cryptography/HashCryptography.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Tolitech.CodeGenerator.Security.Cryptography
+{
+    public sealed class HashCryptography
+    {
+        /// <summary>
+        /// Method to encrypt a text.
+        /// </summary>
+        /// <param name="plainText">plain text</param>
+        /// <param name="salt">salt</param>
+        /// <returns>Encrypted text</returns>
+        public static string Encrypt(string plainText, string salt)
+        {
+            string data = null;
+
+            using (var provider = SHA512.Create())
+            {
+                byte[] cryptoByte = provider.ComputeHash(Encoding.ASCII.GetBytes(plainText + salt));
+                data = BitConverter.ToString(cryptoByte).Replace("-", "").ToLower();
+            }
+
+            return data;
+        }
+    }
+}
